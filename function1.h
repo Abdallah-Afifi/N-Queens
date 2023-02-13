8203e313d3a641737c9d2536b3a00c48411428b2
AbdallahFathi <abdallahfathi@aucegypt.edu>
2023-02-13 20:13:08 +0200

    update

--- function1.h
+++ function1.h
@@ -1,6 +1,7 @@
 #include <iostream>
+#define N 4
 using namespace std;
-void printBoard(int board[N][N]);
-bool isValid(int board[N][N], int row, int col);
-bool solveNQueen(int board[N][N], int col);
+void printBoard(int board [N][N]);
+bool isValid(int board [N][N], int row, int col);
+bool solveNQueen(int board [N][N], int col);
 bool checkSolution();
